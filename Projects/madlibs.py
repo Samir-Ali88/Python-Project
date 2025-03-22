e0b2ac8339192492007a42e0f627a13bd74b6da7
Imam Samir Ali <samirali.tech88@gmail.com>
2025-03-23 00:29:02 +0600

    Delete Projects directory

--- Projects/madlibs.py
+++
@@ -1,7 +0,0 @@
-# print("In this practice session we will practice madlibs")
-
-colour= input("Enter a colour name: ")
-adjective=input("Use an adjective: ")
-name=input("Enter a name: ")
-
-print(f"Roses are {colour}\nYou are {adjective}\nI love {name}")
